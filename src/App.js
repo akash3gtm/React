f372ed1c640106d5767a55cabf15241a99e656eb
Akash Gautam <akashgautam@geekyants.com>
2022-09-01 17:00:31 +0530

    changed the functional approach to class based one

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    this.state = {title : '', greet : ''};
+    //initialise the states
+  }
+
+  componentDidMount(){
+    this.update = setInterval(update(this.state), 60*1000);
+  }
+
+  render(){
+    const {screen} = this.state;
+    return <React.Fragment>
+    <h2 className={title}>
+      {greet}
+    </h2>
+    </React.Fragment> ;
+  }
+
+}
+
+function update(state) {
   const hrs = new Date().getHours();
-  let greet = '';
-  let title = '';
   if (hrs < 12){
-    title = 'Morning';
-    greet = 'Good Morning from React. You are having coffee with me.';
+    state.title = 'Morning';
+    state.greet = 'Good Morning from React. You are having coffee with me.';
   }
   else if (hrs >= 12 && hrs <= 17){
-    title = 'Noon';
-    greet = 'Good Afternoon from React. Wanna lunch with me?';
+    state.title = 'Noon';
+    state.greet = 'Good Afternoon from React. Wanna lunch with me?';
   }
   else if (hrs >= 17 && hrs <= 24){
-    title = 'Evening';
-    greet = 'Good Evening from React. Lets drink together.';
+    state.title = 'Evening';
+    state.greet = 'Good Evening from React. Lets drink together.';
   }
-  return <React.Fragment>
-  <h2 className={title}>
-    {greet}
-  </h2>
-  </React.Fragment> ;
 }
+
 
 export default App;
